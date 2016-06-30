1719ffc7a6127d1aa4b300eac91edac37007b82e
Jael <jaele.jones@gmail.com>
2016-06-30 09:15:26 -0400

    Classes added

---
+++ Library/DVD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class DVD: Media
+
+    {
+        public override int RentalLength
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
